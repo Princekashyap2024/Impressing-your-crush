4e66db046721d0bbbeb97a234c03e37da87aeb71
Princekashyap2024 <amitkumarraikwar92@gmail.com>
2024-03-20 17:18:00 +0530

    fix

--- app/yes/page.tsx
+++ app/yes/page.tsx
@@ -1,21 +1,35 @@
+"use client";
+
+import { useClickContext } from "@/contexts/click-context";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Page() {
-    return <div className="container">
-        <Link href="https://tenor.com/search/mochi-gifs" target="_blank">
-      <img src="https://media1.tenor.com/m/A4Lvk69JMJkAAAAC/mochi-cat.gif" alt="" />
-    </Link>
-        {/* <div
-            className="tenor-gif-embed"
-            data-postid="253027946666209433"
-            data-share-method="host"
-            data-aspect-ratio="1.37853"
-            data-width="100%"
-        >
-            <Link
-                href="https://tenor.com/view/mochi-cat-mochi-and-goma-goma-and-peach-mochi-mochi-peach-cat-gif-gif-253027946666209433"
-            >Mochi Cat GIF</Link>from <Link href="https://tenor.com/search/mochi-gifs">Mochi GIFs</Link>
-        </div> */}
-        <h1>Hehehehe, I knew it! 😘</h1>
+  const clickContext = useClickContext();
+  async function handleSave() {
+    const res = await fetch("https://api.ipify.org?format=json");
+    const data = await res.json();
+    await fetch("/api/", {
+      method: "POST",
+      body: JSON.stringify({
+        response: clickContext.response,
+        ip: data["ip"],
+      }),
+    });
+  }
+
+  useEffect(() => {
+    if (clickContext.response.length > 0) handleSave();
+  }, []);
+  return (
+    <div className="container">
+      <Link href="https://tenor.com/search/mochi-gifs" target="_blank">
+        <img
+          src="https://media1.tenor.com/m/A4Lvk69JMJkAAAAC/mochi-cat.gif"
+          alt=""
+        />
+      </Link>
+      <h1>Hehehehe, I knew it! 😘</h1>
     </div>
+  );
 }
--- components/ResponseButton.tsx
+++ components/ResponseButton.tsx
@@ -11,24 +11,9 @@ export function ResponseButton({
   children: string;
 }) {
   const clickContext = useClickContext();
-  function handleClick() {
-    if (children.toLowerCase().includes("yes")) {
-      (async () => {
-        const res = await fetch("https://api.ipify.org?format=json");
-        const data = await res.json();
-        clickContext.response.push("Yes")
-        await fetch("/api/", {
-          method: "POST",
-          body: JSON.stringify({
-            response: clickContext.response,
-            ip: data["ip"],
-          }),
-        });
-      })();
-    } else clickContext.addResponse(children);
-  }
+  
   return (
-    <Link href={href} onClick={handleClick}>
+    <Link href={href} onClick={() => clickContext.addResponse(children)}>
       {children}
     </Link>
   );
